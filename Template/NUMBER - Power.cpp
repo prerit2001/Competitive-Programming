3ec452886283521efb8b7fb83e97fd80d041b623
Prerit Kumar Jha <preritkrjha@gmail.com>
2021-09-08 22:41:50 +0530

    Update NUMBER - Power.cpp

--- Template/NUMBER - Power.cpp
+++ Template/NUMBER - Power.cpp
@@ -1,17 +1,13 @@
-int power(int a,int b,int mod){
-    int res = 1;
-    while(1){
-		if(b==0){
-			break;
-		}
-		int cnt = 1;
-		int curr = a % mod;
-		while(cnt * 2 < b){
-			curr = (curr%mod*curr%mod)%mod;
-			cnt *= 2;
-		}
-		res = (res%mod*curr%mod)%mod;
-		b -= cnt;
-	}
-	return (res%mod+mod)%mod;
-};
+int power(int x,int y, int mod){
+    int res = 1;  
+    x = x % mod;
+    if (x == 0) return 0; 
+ 
+    while (y > 0){
+        if (y & 1)
+            res = (res*x) % mod;
+        y = y>>1; 
+        x = (x*x) % mod;
+    }
+    return res;
+}
