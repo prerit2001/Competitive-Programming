0af6cf547e23f2dc709af5411301a75d1a7d2f4c
Prerit Kumar Jha <preritkrjha@gmail.com>
2021-12-09 23:03:06 +0530

    UPDATE

--- CSES/Advanced Technique/Cut_and_Paste.cpp
+++ CSES/Advanced Technique/Cut_and_Paste.cpp
@@ -15,9 +15,8 @@ ll power(ll x, ll y, ll mod) {
 }
 void solve();
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     ll t = 1;
     // cin >> t;
     while (t--) {
@@ -26,294 +25,136 @@ int main() {
     return 0;
 }
 
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-
-int getRand(int l, int r) {
-    uniform_int_distribution<int> uid(l, r);
-    return uid(rng);
-}
-
-typedef struct node {
-    int val, prior, size;
-    struct node *l, *r;
-} node;
-
-typedef node *pnode;
-
-int sz(pnode t) { return t ? t->size : 0; }
-
-void upd_sz(pnode t) {
-    if (t) {
-        t->size = sz(t->l) + 1 + sz(t->r);
-    }
-}
-
-pnode find(pnode t, int key) {
-    if (t == NULL || t->val == key) {
-        return t;
-    }
-
-    return find(t->val < key ? t->r : t->l, key);
-}
-
-void split(pnode t, pnode &l, pnode &r, int key) {
-    if (!t) {
-        l = r = NULL;
-    } else if (t->val <= key) {
-        split(t->r, t->r, r, key);
-        l = t;
-    } else {
-        split(t->l, l, t->l, key);
-        r = t;
-    }
-
-    upd_sz(t);
-}
-
-void merge(pnode &t, pnode l, pnode r) {
-    if (!l || !r) {
-        t = l ? l : r;
-    } else if (l->prior > r->prior) {
-        merge(l->r, l->r, r);
-        t = l;
-    } else {
-        merge(r->l, l, r->l);
-        t = r;
-    }
-
-    upd_sz(t);
-}
-
-void insert(pnode &t, pnode it) {
-    if (!t) {
-        t = it;
-    } else if (it->prior > t->prior) {
-        split(t, it->l, it->r, it->val);
-        t = it;
-    } else {
-        insert(t->val <= it->val ? t->r : t->l, it);
-    }
-
-    upd_sz(t);
-}
-
-void erase(pnode &t, int key) {
-    pnode p = find(t, key);
-    if (p == NULL) {
-        return;
-    }
-
-    pnode temp = t;
-    merge(t, p->l, p->r);
-    free(temp);
-
-    upd_sz(t);
-}
-
-pnode init(int val) {
-    pnode ret = (pnode)malloc(sizeof(node));
-    ret->val = val;
-    ret->size = 1;
-    ret->prior = rand();
-    ret->l = ret->r = NULL;
-    return ret;
-}
-
-int order_of_key(pnode &t, int val)  // 1 indexed
-{
-    pnode l, r, mid;
-    split(t, l, r, val - 1);
-    split(r, mid, r, val);
-    int ans = -1;
-    if (sz(mid) == 1) ans = 1 + sz(l);
-    merge(r, mid, r);
-    merge(t, l, r);
-    return ans;
-}
+mt19937 mt;
+struct node {
+    ll p, v, s;
+    node *l, *r;
+    node(ll x) : p(x), v(mt()), s(1) { l = r = nullptr; }
+    void pull() { s = 1 + (l ? l->s : 0) + (r ? r->s : 0); }
+};
 
 class Treap {
    public:
-    struct data {
-        int priority, val, cnt;
-        data *l, *r;
-
-        data() { val = 0, cnt = 0, l = NULL, r = NULL; }
-
-        data(int _val) {
-            val = _val, cnt = 1;
-            l = NULL, r = NULL;
-            priority = getRand(1, 2e9);
-        }
-    };
-
-    typedef struct data *node;
-
-    node head;
-
-    Treap() : head(0) {}
-
-    int cnt(node cur) { return cur ? cur->cnt : 0; }
-
-    void updateCnt(node cur) {
-        if (cur) cur->cnt = cnt(cur->l) + cnt(cur->r) + 1;
+    node* merge(node* a, node* b) {
+        if (!a || !b) return a ?: b;
+        if (a->v < b->v)
+            return a->r = merge(a->r, b), a->pull(), a;
+        else
+            return b->l = merge(a, b->l), b->pull(), b;
     }
 
-    void push(node cur)  // Lazy Propagation
-    {
-        ;
-    }
-
-    void combine(node &cur, node l, node r) {
-        if (!l) {
-            cur = r;
+    void split(node* cur, ll k, node*& a, node*& b) {
+        if (!cur) {
+            a = b = nullptr;
             return;
         }
-        if (!r) {
-            cur = l;
-            return;
-        }
-        // Merge Operations like in segment tree
-    }
-
-    void reset(node &cur)  // To reset other fields of cur except value and cnt
-    {
-        if (!cur) return;
-    }
-
-    void operation(node &cur) {
-        if (!cur) return;
-        reset(cur);
-        combine(cur, cur->l, cur);
-        combine(cur, cur, cur->r);
-    }
-
-    void splitPos(node cur, node &l, node &r, int k, int add = 0) {
-        if (!cur) return void(l = r = 0);
-        push(cur);
-        int idx = add + cnt(cur->l);
-        if (idx <= k)
-            splitPos(cur->r, cur->r, r, k, idx + 1), l = cur;
+        if (cur->p < k)
+            a = cur, split(a->r, k, a->r, b), a->pull();
         else
-            splitPos(cur->l, l, cur->l, k, add), r = cur;
-        updateCnt(cur);
-        operation(cur);
+            b = cur, split(b->l, k, a, b->l), b->pull();
     }
 
-    void split(node cur, node &l, node &r, int k) {
-        if (!cur) return void(l = r = 0);
-        push(cur);
-        int idx = cur->val;
-        if (idx <= k)
-            split(cur->r, cur->r, r, k), l = cur;
+    void insert(node*& cur, ll x) {
+        node *l, *r;
+        split(cur, x, l, r);
+        cur = merge(merge(l, new node(x)), r);
+    }
+
+    void erase(node*& cur, ll x) {
+        node *l, *m, *r;
+        split(cur, x, l, m);
+        split(m, x + 1, m, r);
+        if (m)
+            cur = merge(merge(l, merge(m->l, m->r)), r);
         else
-            split(cur->l, l, cur->l, k), r = cur;
-        updateCnt(cur);
-        operation(cur);
+            cur = merge(l, r);
     }
 
-    void merge(node &cur, node l, node r) {
-        push(l);
-        push(r);
-        if (!l || !r)
-            cur = l ? l : r;
-        else if (l->priority > r->priority)
-            merge(l->r, l->r, r), cur = l;
-        else
-            merge(r->l, l, r->l), cur = r;
-        updateCnt(cur);
-        operation(cur);
-    }
-
-    void insert(int val) {
-        if (!head) {
-            head = new data(val);
-            return;
-        }
-        node l, r, mid, mid2, rr;
-        mid = new data(val);
-        split(head, l, r, val - 1);
-        merge(l, l, mid);
-        split(r, mid2, rr, val);
-        merge(head, l, rr);
-    }
-
-    void erase(int val) {
-        node l, r, mid;
-        split(head, l, r, val - 1);
-        split(r, mid, r, val);
-        merge(head, l, r);
-    }
-
-    void inorder(node cur) {
-        if (!cur) return;
-        push(cur);
-        inorder(cur->l);
-        cout << cur->val << " ";
-        inorder(cur->r);
-    }
-
-    void inorder() {
-        inorder(head);
-        cout << endl;
-    }
-
-    void clear(node cur) {
-        if (!cur) return;
-        clear(cur->l), clear(cur->r);
-        delete cur;
-    }
-
-    void clear() { clear(head); }
-
-    int find_by_order(int x)  // 1 indexed
-    {
-        if (!x) return -1;
-        x--;
-        node l, r, mid;
-        splitPos(head, l, r, x - 1);
-        splitPos(r, mid, r, 0);
-        int ans = -1;
-        if (cnt(mid) == 1) ans = mid->val;
-        merge(r, mid, r);
-        merge(head, l, r);
+    ll strictly_less_than_custom(node*& cur, ll x) {
+        node *l, *r;
+        split(cur, x, l, r);
+        ll ans = (l ? l->s : 0);
+        cur = merge(l, r);
         return ans;
     }
 
-    int order_of_key(int val)  // 1 indexed
-    {
-        node l, r, mid;
-        split(head, l, r, val - 1);
-        split(r, mid, r, val);
-        int ans = -1;
-        if (cnt(mid) == 1) ans = 1 + cnt(l);
-        merge(r, mid, r);
-        merge(head, l, r);
+    // let max value of a[i] be 1e10
+    ll find_by_order2(node*& cur, ll x, ll n) {  // 1 indexed // (logn)^2
+        cout << x << '\n';
+        if (x < 1 or x > n) return -1;
+        x--;
+        cout << x << '\n';
+        ll left = 1, right = 1e9 + 1;
+        ll ans = -1;
+        while (left < right) {
+            ll mid = (left + right) >> 1;
+            ll val = strictly_less_than_custom(cur, mid);
+            if (val > x) {
+                right = mid;
+            } else {
+                ans = mid;
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    void splitPos(node* cur, ll k, node*& a, node*& b, ll add = 0) {
+        if (!cur) {
+            a = b = nullptr;
+            return;
+        }
+        ll idx = add + (cur->l ? cur->l->s : 0);
+        if (idx < k)
+            a = cur, splitPos(a->r, k, a->r, b, idx + 1), a->pull();
+        else
+            b = cur, splitPos(b->l, k, a, b->l, add), b->pull();
+    }
+
+    ll find_by_order(node*& cur, ll x, ll n) {
+        node *l, *m, *r;
+        splitPos(cur, x, l, m);
+        splitPos(m, 1, m, r);
+        ll ans = -1;
+        if (m) {
+            ans = m->p;
+        }
+        cur = merge(l, merge(m, r));
+        return ans;
+    }
+
+    ll order_of_key(node*& cur, ll x) {  // 1 indexed
+        node *l, *m, *r;
+        split(cur, x, l, m);
+        split(m, x + 1, m, r);
+        ll ans = -1;
+        if (m) {  // possible
+            ans = 1 + (l ? l->s : 0);
+        }
+        cur = merge(l, merge(m, r));
         return ans;
     }
 };
 
 void solve() {
-    // Treap t;
-    pnode t;
-    while (true) {
-        ll i, x;
-        cin >> i;
-        if (i == -1) break;
-        cin >> x;
-        if (i == 1) {
-            insert(t, init(x));
-        }
-        if (i == 2) {
-            erase(t, x);
-        }
-        if (i == 3) {
-            cout << "-";
-            // cout << order_of_key(t, x);
-            cout << '\n';
-        }
-        // if (i == 4) {
-        //     cout << t.find_by_order(x);
-        //     cout << '\n';
-        // }
+    ll n, m;
+    cin >> n >> m;
+    string s;
+    cin >> s;
+    Treap t;
+    node* root = nullptr;
+    for (ll i = 0; i < n; i++) {
+        t.insert(root, i);
+    }
+    while (m--) {
+        ll x, y;
+        cin >> x >> y;
+        node *l, *m, *r;
+        t.splitPos(root, x - 1, l, m);
+        t.splitPos(m, y - x + 1, m, r);
+        root = t.merge(t.merge(l, r), m);
+    }
+    for (ll i = 0; i < n; i++) {
+        cout << s[t.find_by_order(root, i, n)];
     }
 }
---
+++ CSES/Advanced Technique/Substring_Reversals.cpp
@@ -0,0 +1,142 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(x) (x).begin(), (x).end()
+typedef long long ll;
+ll power(ll x, ll y, ll mod) {
+    ll res = 1;
+    x = x % mod;
+    if (x == 0) return 0;
+    while (y > 0) {
+        if (y & 1) res = (res * x) % mod;
+        y = y >> 1;
+        x = (x * x) % mod;
+    }
+    return res;
+}
+void solve();
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
+
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+struct node{
+    ll value;
+    ll sz;
+    ll prior;
+    ll sum;
+    node *l;
+    node *r;
+    bool rev;
+    node(ll value){
+        this -> value = value;
+        this -> sz = 1;
+        this -> prior = rng();
+        this -> sum = value;
+        this -> rev = 0;
+        this -> l = nullptr;
+        this -> r = nullptr;
+    }
+};
+
+ll sz(node *x){
+    return x ? x -> sz : 0ll;
+}
+
+ll sum(node *x){
+    return x ? x -> sum : 0ll;
+}
+
+void recalc(node *x){
+    assert(x);
+    x -> sz = 1 + sz(x -> l) + sz(x -> r);
+    x -> sum = x -> value + sum(x -> l) + sum(x -> r);
+}
+
+void push(node* x) {
+  if (!x) return;
+  if (x->rev) {
+    swap(x->l, x->r);
+    if (x->l) x->l->rev ^= true;
+    if (x->r) x->r->rev ^= true;
+    x->rev = false;
+  }
+}
+
+node* merge(node* l, node* r) {
+    push(l);
+    push(r);
+    if (!l) return r;
+    if (!r) return l;
+    if (l->prior < r->prior) {
+        l->r = merge(l->r, r);
+        recalc(l);
+        push(l);
+        return l;
+    }
+    else{
+        r->l = merge(l, r->l);
+        recalc(r);
+        push(r);
+        return r;
+    }
+}
+
+
+// split(x, k) -> { (1,2,3...,n) -> {(1, 2, 3, ..k),(k + 1, .. n)}}
+pair<node*, node*> split(node* x, ll k){
+    push(x);
+    if (!x) {
+        return {nullptr, nullptr};
+    }
+    if (k <= sz(x->l)) {
+        pair<node*, node*> p = split(x->l, k);
+        node* l = p.first;
+        node* r = p.second;
+        x->l = r;
+        recalc(x);
+        push(x);
+        return make_pair(l, x);
+    }
+    else{
+        pair<node*, node*> p = split(x->r, k - sz(x->l) - 1);
+        node* l = p.first;
+        node* r = p.second;
+        x->r = l;
+        recalc(x);
+        push(x);
+        return make_pair(x, r);
+    }
+}
+
+void solve() {
+    ll n; cin >> n;
+    ll q; cin >> q;
+    string v; cin >> v;
+    // for(auto &it : v) cin >> it;
+    node* root = nullptr;
+    for(ll it = 0; it < n; it ++) root = merge(root, new node(it));
+    while(q --){
+        ll type, l, r; cin >> l >> r;
+        pair<node*, node*> p1 = split(root, l - 1);
+        pair<node*, node*> p2 = split(p1.second, r - l + 1);
+        p2.first -> rev ^= 1;
+        root = merge(p1.first, p2.first);
+        root = merge(root, p2.second);
+    }
+    for(ll i = 1; i <= n; i ++){
+        pair<node*, node*> p1 = split(root, i - 1);
+        pair<node*, node*> p2 = split(p1.second, 1);
+        cout << v[p2.first -> value] ;
+        root = merge(p1.first, p2.first);
+        root = merge(root, p2.second);
+    }
+}
