a0462d7aa41aa8a81bd55270b9ace764578dea22
Prerit Kumar Jha <preritkrjha@gmail.com>
2021-12-30 19:00:00 +0530

    Ratings Updated

---
+++ Striver/57NthRootofaNumber.cpp
@@ -0,0 +1,43 @@
+/*Nth Root of a Number using Binary Search*/
+-----------------------------------------------------------------------------
+/*
+	M1: Binary Search
+	O(logn * N) time and O(1) space
+*/
+	#include <bits/stdc++.h>
+	using namespace std;
+	double multiply(double number, int n) {
+	    double ans = 1.0;
+	    for(int i = 1;i<=n;i++) {
+	        ans = ans * number;
+	    }
+	    return ans; 
+	}
+
+	double getNthRoot(int n, int m) {
+	    double low = 1;
+	    double high = m;
+	    double eps = 1e-6; 
+	    
+	    while((high - low) > eps) {
+	        double mid = (low + high) / 2.0; 
+	        if(multiply(mid, n) < m) {
+	            low = mid; 
+	        }
+	        else {
+	            high = mid; 
+	        }
+	    }
+	    
+	    cout << low << " " << high << endl; 
+	    
+	    // just to check
+	    cout << pow(m, (double)(1.0/(double)n)); 
+	}
+	int main() {
+		int n, m;
+		cin >> n >> m; 
+		getNthRoot(n, m); 
+		return 0;
+	}
+-----------------------------------------------------------------------------
