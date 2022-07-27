8f338a9aa81e34c54ce8b2f0daf2137d0ff66782
Prerit Kumar Jha <preritkrjha@gmail.com>
2022-07-27 20:43:31 +0530

    UPDATE

--- Striver/061. MedianTwoSortedArray.cpp
+++ Striver/061. MedianTwoSortedArray.cpp
@@ -15,6 +15,8 @@
 	M3: Advance Binary Search
 	O(log(n1 + n2)) time and O(1) space
 
+	https://www.youtube.com/watch?v=ws98ud9uxl4
+
 	Approach :
 
 	We will do a binary search in one of the arrays which have a minimum size among the two. 
@@ -30,42 +32,51 @@
 	If partitioning is not valid, move ranges. When l1>r2, move left and perform the 
 	above operations again. When l2>r2, move right and perform the above operations.
 */
-	#include<bits/stdc++.h>
-	using namespace std;
-
-	float median(int num 1[],int num2[],int m,int n) {
-	    if(m>n)
-	        return median(nums2,nums1,n,m);//ensuring that binary search happens on minimum size array
+	class Solution {
+	public:
+	    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
 	        
-	    int low=0,high=m,medianPos=((m+n)+1)/2;
-	    while(low<=high) {
-	        int cut1 = (low+high)>>1;
-	        int cut2 = medianPos - cut1;
+	        int n = nums1.size();
+	        int m = nums2.size();
 	        
-	        int l1 = (cut1 == 0)? INT_MIN:nums1[cut1-1];
-	        int l2 = (cut2 == 0)? INT_MIN:nums2[cut2-1];
-	        int r1 = (cut1 == m)? INT_MAX:nums1[cut1];
-	        int r2 = (cut2 == n)? INT_MAX:nums2[cut2];
-	        
-	        if(l1<=r2 && l2<=r1) {
-	            if((m+n)%2 != 0)
-	                return max(l1,l2);
-	            else 
-	                return (max(l1,l2)+min(r1,r2))/2.0;
+	        // nums1 len < nums2 len 
+	        if(n > m){
+	            return findMedianSortedArrays(nums2, nums1);
 	        }
-	        else if(l1>r2) high = cut1-1;
-	        else low = cut1+1;
+	        
+	        int low1 = 0, high1 = n;
+	        int medianId = (n + m + 1) / 2;
+	        while(low1 <= high1){
+	            
+	            int mid1 = low1 + (high1 - low1) / 2; // avoid overflow -> corner case
+	            int mid2 = medianId - mid1;
+	            
+	            int max1 = mid1 == n ? INT_MAX: nums1[mid1];
+	            int min1 = mid1 == 0 ? INT_MIN: nums1[mid1 - 1];
+	            
+	            int max2 = mid2 == m ? INT_MAX: nums2[mid2];
+	            int min2 = mid2 == 0 ? INT_MIN: nums2[mid2 - 1];
+	            
+	            if(max1 >= min2 and max2 >= min1){
+	                if((n + m) % 2 == 0){
+	                    return (max(min1, min2) + min(max1, max2)) / 2.0;
+	                }
+	                else{
+	                    return max(min1, min2);
+	                }
+	            }
+	            else if(max1 < min2){
+	                low1 = mid1 + 1;
+	            }
+	            else{
+	                high1 = mid1 - 1;
+	            }
+	            
+	        }
+	         
+	        assert(0); // not possible for code to react this line, but if code exactly reached this line that means there is some logical error in code;
+	        return -1;
+	        
 	    }
-	    return 0.0;
-	}
-
-	int main() {
-	    int nums1[] = {1,4,7,10,12};
-	    int nums2[] = {2,3,6,15};
-	    int m = sizeof(nums1)/sizeof(nums1[0]);
-	    int n = sizeof(nums2)/sizeof(nums2[0]);
-	    cout<<"The Median of two sorted arrays is"<<fixed<<setprecision(5)
-	    <<median(nums1,nums2,m,n);
-	    return 0;
-	}
+	};
 ---------------------------------------------------------------------------------
